f23e9b0f0b0e121e776b097ac283ba9ee276f676
abdullah97 <abdullahalshawa@gmail.com>
2023-02-07 20:47:39 -0800

    PyTest method to see if the output is working correctly (printing hello world)

--- pages/tests.py
+++ pages/tests.py
@@ -1,5 +1,12 @@
-from django.test import TestCase
+import os
 
-# Create your tests here.
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_project.settings")
+from .views import homePageView
+from django.test import RequestFactory
+
+
 def test_my_test():
-    assert(True,True)
+    request = RequestFactory().get('/')
+    response = homePageView(request)
+    #Success return code
+    assert response.status_code == 200
