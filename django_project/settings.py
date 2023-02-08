5d8e7057cb39f695063368b2555799abe3a8710e
abdullah97 <abdullahalshawa@gmail.com>
2023-02-08 11:40:45 -0800

    Docker

--- django_project/settings.py
+++ django_project/settings.py
@@ -19,13 +19,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
+#SECRET_KEY = str(os.getenv('SECRET_KEY'))
+# SECURITY WARNING: don't run with debug turned on in production!
+#DEBUG = True
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 ALLOWED_HOSTS = []
-
-
 # Application definition
 
 INSTALLED_APPS = [
