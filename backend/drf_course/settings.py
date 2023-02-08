3512edc0bdf8ff2537e8ea9eaf170badd0fd487e
abdullah97 <abdullahalshawa@gmail.com>
2023-02-08 13:26:19 -0800

    Load the environment for the secret key, debug, and allowed hosts.

--- backend/drf_course/settings.py
+++ backend/drf_course/settings.py
@@ -21,13 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = int(os.environ.get("DEBUG", default=0))
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
