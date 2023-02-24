5e41944a006b87f2a56ee7e154ae00ee589152e3
abdullah97 <abdullahalshawa@gmail.com>
2023-02-24 09:35:48 -0800

    Added views

--- django_project/urls.py
+++ django_project/urls.py
@@ -11,11 +11,11 @@ from pages import views as poll_views
 urlpatterns = [
     #path("", views.index, name='index'),
     #Home
-    path("", include("pages.urls")),
+    path('', include('pages.urls')),
     #Django adminstration
     path('admin/', admin.site.urls),
-    path('create/', poll_views.createPageView, name="create"),
-    #path('vote/<poll_id>/', poll_views.home, name="home"),
+    path('create/', poll_views.createPageView, name='create'),
+    #path('vote', poll_views.votePageView, name='vote'),
     #path('', pages.Templates)
     #path('about/', views.about, name='about'),
 ]
