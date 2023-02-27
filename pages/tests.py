0bd2a1f25afe6f0f8976aa224ab2927eaa4bc367
abdullah97 <abdullahalshawa@gmail.com>
2023-02-27 09:47:48 -0800

    Test the create.html view

--- pages/tests.py
+++ pages/tests.py
@@ -1,12 +1,18 @@
 import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_project.settings")
-from .views import homePageView
+from .views import homePageView, createPageView
 from django.test import RequestFactory
-
+from django.urls import reverse
 
 def test_my_test():
     request = RequestFactory().get('/')
     response = homePageView(request)
     #Success return code
     assert response.status_code == 200
+def test_create_view():
+    request = RequestFactory().get('/')
+    response = createPageView(request)
+
+    assert response.status_code == 200
+    #assert response == 'create.html'
